ee684976ee6adb47887b9f9e21e93711fef464db
Yateesh S <keenyateesh@gmail.com>
2025-11-22 20:42:10 +0530

    Add Sort functionality to sort by various parameters

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import CoinCard from "./components/CoinCard";
 import LimitSelector from "./components/LimitSelector";
 import FilterInput from "./components/FilterInput";
+import SortSelector from "./components/SortSelector";
 
 const API_URL = "https://api.coingecko.com/api/v3/coins/markets";
 
@@ -10,29 +11,55 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [limit, setLimit] = useState(10);
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState("");
+  const [sortBy, setSortBy] = useState("market_cap_desc");
 
   useEffect(() => {
     const fetchCoins = async () => {
       try {
-        const response = await fetch(API_URL + `?vs_currency=usd&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`);
+        const response = await fetch(
+          API_URL +
+            `?vs_currency=usd&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
+        );
         if (!response.ok) throw new Error("Failed to load data");
         const data = await response.json();
-        setCoins(data)
+        setCoins(data);
       } catch (err) {
-        setError(err.message)
-        console.error(err.message)
+        setError(err.message);
+        console.error(err.message);
       } finally {
         setLoading(false);
       }
     };
 
     fetchCoins();
-  },[limit]);
+  }, [limit]);
 
-  const filteredCoins = coins.filter((coin) => {
-    return coin.name.toLowerCase().includes(filter.toLowerCase()) || coin.symbol.toLowerCase().includes(filter.toLowerCase())
-  });
+  const filteredCoins = coins
+    .filter((coin) => {
+      return (
+        coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(filter.toLowerCase())
+      );
+    })
+    .sort((a, b) => {
+      switch(sortBy) {
+        case 'market_cap_desc':
+          return  b.market_cap - a.market_cap;
+        case 'market_cap_asc':
+          return  a.market_cap - b.market_cap;
+        case 'price_desc':
+          return b.current_price - a.current_price;
+        case 'price_asc':
+          return a.current_price - b.current_price;
+        case 'change_desc':
+          return b.price_change_percentage_24h - a.price_change_percentage_24h;
+        case 'change_asc':
+          return a.price_change_percentage_24h - b.price_change_percentage_24h;
+        default:
+          return 0;
+      }
+    });
 
   return (
     <div>
@@ -40,15 +67,18 @@ function App() {
       {loading && <p>Loading Market Data...</p>}
       {!loading && error && <div className="error">{error}</div>}
       <div className="top-controls">
-      <FilterInput filter={filter} onFilterChange={setFilter} />
-      <LimitSelector limit={limit} onLimitChange={setLimit} />
+        <FilterInput filter={filter} onFilterChange={setFilter} />
+        <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
+        <LimitSelector limit={limit} onLimitChange={setLimit} />
       </div>
 
       {!loading && !error && (
         <main className="grid">
-          {filteredCoins.length > 0 ? filteredCoins.map((coin) => (
-            <CoinCard coin={coin} key={coin.id} />
-          )) : <p>No match found</p>}
+          {filteredCoins.length > 0 ? (
+            filteredCoins.map((coin) => <CoinCard coin={coin} key={coin.id} />)
+          ) : (
+            <p>No match found</p>
+          )}
         </main>
       )}
     </div>
