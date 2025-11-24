4f61898782bc38be004f7cc086d6edb47ab249d0
Yateesh S <keenyateesh@gmail.com>
2025-11-24 17:10:29 +0530

    Refactor CoinCard component to improve price change display and ensure proper link wrapping

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -1,6 +1,9 @@
+import { Link } from "react-router";
+
 const CoinCard = ({coin, currency}) => {
   const locale = currency === 'inr' ? 'en-IN' : 'en-US';
-    return ( 
+    return (
+      <Link to={`/coin/${coin.id}`}>
         <div className='coin-card'>
               <div className="coin-header">
                 <img src={coin.image} alt={coin.name} className="coin-image" />
@@ -10,9 +13,10 @@ const CoinCard = ({coin, currency}) => {
                 </div>
               </div>
               <p><b>Price: {currency === 'inr' ? <span>&#8377;</span> : <span>&#36;</span>}{coin.current_price.toLocaleString(locale)}</b></p>
-              <p className={coin.price_change_percentage_24h >= 0 ? 'positive' : 'negative'}><b>{coin.price_change_percentage_24h >= 0 && '+'}{coin.price_change_percentage_24h.toFixed(2)}%</b></p>
+              <p className={coin.price_change_percentage_24h >= 0 ? 'positive' : 'negative'}><b>{(coin.price_change_percentage_24h >= 0 && '+') || ''}{coin.price_change_percentage_24h?.toFixed(2) || ''}%</b></p>
               <p><b>Market Cap:</b> {currency === 'inr' ? <span>&#8377;</span> : <span>&#36;</span>}{coin.market_cap.toLocaleString(locale)}</p>
             </div>
+        </Link>
      );
 }
  
