acc3cfbbc0d514d7e56574d6ec9abdb0ac061ad7
Yateesh S <keenyateesh@gmail.com>
2025-11-24 17:10:41 +0530

    Update LimitSelector options to include 100 and remove 75

--- src/components/LimitSelector.jsx
+++ src/components/LimitSelector.jsx
@@ -5,7 +5,7 @@ const LimitSelector = ({limit, onLimitChange}) => {
         <option value="10">10</option>
         <option value="25">25</option>
         <option value="50">50</option>
-        <option value="75">75</option>
+        <option value="100">100</option>
       </select>
       </div> );
 }
